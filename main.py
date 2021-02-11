dfe350c352a6bde4e03ca758269f264c182a62f7
zxcayumi <zxcayumi@qq.com>
2021-02-11 12:38:32 +0800

    调节参数

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ import util
 
 parser = argparse.ArgumentParser(description='pytorch version of GraphSAGE by Xuechen Zhao')
 parser.add_argument('--dataSet', type=str, default='cora')
-parser.add_argument('--epoch', type=int, default=5)
+parser.add_argument('--epoch', type=int, default=6)
 parser.add_argument('--epochSize', type=int, default=50)
 parser.add_argument('--batchSize', type=int, default=16)
-parser.add_argument('--device', type=str, default='cpu')
+parser.add_argument('--device', type=str, default='cuda')
 args = parser.parse_args()
 
 util.device = args.device
 dc = context.data_center(args.dataSet)
 
+sample_num = [15, 10] # number of neighbor samples per order
 input_dim = len(dc.data.features[0])
 output_dim = len(dc.data.label_map)
-sample_num = [10, 10] # number of neighbor samples per order
 orders = len(sample_num) # number of orders
 
 model = models.GraphSage(input_dim, output_dim, orders).to(util.device)
@@ -59,9 +59,7 @@ def train():
                 e, i, lossy, accuracy))
         
         eval(context.set_type.vals)
-    
-    eval(context.set_type.tests)
-
+        eval(context.set_type.tests)
 
 def eval(type = context.set_type.vals):
     model.eval()
@@ -71,7 +69,7 @@ def eval(type = context.set_type.vals):
         prediction = model(sample_nodes_feature)
         corrects_index = prediction.max(1)[1]
         accuracy = torch.eq(corrects_index, sample_nodes_labels).float().mean().item()
-        print('{:}==>accuracy:{:.4f}'.format(type.name, accuracy))
+        print('{:5}==>accuracy:{:.4f}'.format(type.name, accuracy))
 
 if __name__ == '__main__':
     train()
